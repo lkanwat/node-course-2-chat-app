c287e4b195bfbcd46039aa69f2aa9387b251fdc2
Lakshya kanwat <lakshyakanwat@Lakshyas-MacBook-Air.local>
2017-07-13 16:44:52 +0530

    Emit new message and create message

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,7 @@ var socket = io();
 socket.on('connect',function () {
   console.log('Connected to the server');
 
-  socket.emit('createMessage', {
-    from: 'Andrew',
-    text: 'Yup, that works for me'
-  });
+
 });
 
 socket.on('disconnect',function () {
